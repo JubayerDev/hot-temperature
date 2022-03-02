9e0b7d871f417b2b7449f2ba8ec66ad0aef39a6b
JubayerDev <ajashes858@gmail.com>
2022-03-02 23:52:54 +0600

    project finished

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
 const API_KEY = `fd2ad76f3f380172e79ed4d3497b4e19`;
 const searchTepmerature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     
     fetch(url)
         .then(res => res.json())
         .then(data => displayTemperature(data))
+    
+    document.getElementById('city-name').value = '';
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
 }
 
 const displayTemperature = temperature => {
-    console.log(temperature);
+    console.log(temperature)
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('whether', temperature.weather[0].main);
+
+    // Display Icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`
+    const iconImg = document.getElementById('weather-icon');
+    iconImg.setAttribute('src', url)
 }
